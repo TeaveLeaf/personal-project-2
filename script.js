57e457eca9c728ce537c616111761fb76e0120c6
TeaveLeaf <116187490+TeaveLeaf@users.noreply.github.com>
2023-05-11 10:44:33 -0700

    Added more visuals

--- script.js
+++ script.js
@@ -4,17 +4,16 @@ function openNav() {
     if(count == 0) {
         document.getElementById("navstuff").style.width = "250px";
         document.getElementById("openclose").style.marginLeft = "250px";
-        // document.getElementById("main").style.marginLeft = "250px";
         count = count + 1;
         console.log("Opened the nav");
     } else if (count == 1) {
         document.getElementById("navstuff").style.width = "0";
         document.getElementById("openclose").style.marginLeft = "0";
-        // document.getElementById("main").style.marginLeft= "0";
         count = count - 1;
         console.log("Closed the nav");
     }
 }
+
 
 let imgcount = 0;
 function left() {
